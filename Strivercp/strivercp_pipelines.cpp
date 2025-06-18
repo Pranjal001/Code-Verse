4ddc564194bf94a6afb7531fe1ebd9e11bfac5c5
Pranjal001 <pranjal11kshir@gmail.com>
2025-06-19 00:12:02 +0530

    general update BS1

---
+++ Strivercp/strivercp_pipelines.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll tsum(ll k)
+{
+    ll sum=0;
+    if((k+1) & 1){
+        sum = ((k)/2) * (k+1);
+    }
+    else sum = ((k+1)/2) * (k);
+
+    return sum;
+}
+
+int main()
+{
+    // for I/O work
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    // Initialisation and input
+    ll n,k;
+    cin>>n>>k;
+    
+    n--; // observe here
+    ll ksum = tsum(k-1);
+
+    if(ksum < n){ cout<<-1; return 0;} // excluding
+
+    ll low=0,high=k-1;
+
+    while(low<=high)
+    {
+        ll mid = low + (high-low)/2;
+        
+        if(ksum - tsum(mid) >= n){
+            low = mid+1;
+        }
+        else high = mid-1;
+    }
+    
+    cout<<k-1-high;
+    return 0;
+}
--- practice.cpp
+++ practice.cpp
@@ -2,31 +2,33 @@
 typedef long long ll;
 using namespace std;
 
-bool tsum(ll k)
+ll tsum(ll k)
 {
     ll sum=0;
     if((k+1) & 1){
         sum = ((k)/2) * (k+1);
     }
-    else sum = ((k)/2) * (k+1);
+    else sum = ((k+1)/2) * (k);
+
+    return sum;
 }
 
 int main()
 {
     // for I/O work
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
 
     // Initialisation and input
     ll n,k;
     cin>>n>>k;
     
-    n--;
+    n--; // observe here
     ll ksum = tsum(k-1);
 
-    if(ksum < n){ cout<<-1; return 0;}
+    if(ksum < n){ cout<<-1; return 0;} // excluding
 
     ll low=0,high=k-1;
 
